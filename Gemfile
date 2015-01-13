92657f03368083ee429531e13212e7fec77fd19b
Daniel X Moore <yahivin@gmail.com>
2015-01-13 11:32:37 -0800

    Removed ical gem

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ ruby '2.0.0'
 source "https://rubygems.org"
 
 gem "activesupport"
-gem "icalendar"
 gem "mandrill-api"
 gem "redis"
 
--- test_invite.rb
+++ test_invite.rb
@@ -1,14 +1,8 @@
-#TODO: ["", ""]
+require "active_support"
+require "active_support/all"
 
-require "./create_invite"
 require "./send_emails"
 
-people = [["Daniel X", "danielx@fogcreek.com"]]
+people = [["Daniel X", "danielx@fogcreek.com"], ["Dr. Example", "doctor@example.com"]]
 
-# Assuming being sent out on Friday morning
-start_time = 5.days.from_now.at_noon
-invite = create_invite start_time
-
-puts invite
-
-send_emails people, invite
+send_emails people
